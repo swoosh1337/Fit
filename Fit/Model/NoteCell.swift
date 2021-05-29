b0d71d1d801fa34ac0f0f8b15043d8eb7abae330
Irakli Grigolia <tazigrigolia@gmail.com>
2021-05-29 19:02:32 +0400

    work on training screen, add core data for persistence

---
+++ Fit/Model/NoteCell.swift
@@ -0,0 +1,16 @@
+//
+//  NoteCell.swift
+//  Fit
+//
+//  Created by Irakli Grigolia on 5/29/21.
+//  Copyright © 2021 Beka Japaridze. All rights reserved.
+//
+
+import UIKit
+
+class NoteCell: UITableViewCell
+{
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+}
+
