ec9ae35f296be27711b8a56076346429d657257e
Irakli Grigolia <igrigolia@wpi.edu>
2021-07-22 13:11:54 +0400

    added missing files

---
+++ Fit/View/Cells/AlbumTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlbumTableViewCell.swift
+//  Myres
+//
+//  Created by Luis Genesius on 30/04/21.
+//
+
+import UIKit
+
+class AlbumTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
