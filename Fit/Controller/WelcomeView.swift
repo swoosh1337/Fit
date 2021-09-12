cc324e6464fe6c906024954c5f61f5f2e3c425a2
Irakli Grigolia <igrigolia@wpi.edu>
2021-09-12 13:29:50 -0400

    deleted fb login due to some bugs

--- Fit/Controller/WelcomeView.swift
+++ Fit/Controller/WelcomeView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import FBSDKLoginKit
 
-class WelcomeView: UIViewController,LoginButtonDelegate  {
+class WelcomeView: UIViewController  {
    
  
     @IBOutlet weak var logIn: UIButton!
@@ -32,50 +32,50 @@ class WelcomeView: UIViewController,LoginButtonDelegate  {
         self.performSegue(withIdentifier: "fbLogin", sender: self)
     }
     
-    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
-        print("sasas")
-    }
+//    func loginButtonDidLogOut(_ loginButton: FBLoginButton) {
+//        print("sasas")
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let token = AccessToken.current,
-                !token.isExpired {
-            let token = token.tokenString
+//        if let token = AccessToken.current,
+//                !token.isExpired {
+//            let token = token.tokenString
+//            
+//            let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
+//                                                     parameters: ["fields":"email,name"],
+//                                                     tokenString: token,
+//                                                     version: nil,
+//                                                     httpMethod: .get)
+//            request.start(completionHandler: {connection,result, error in
+//                print("\(result)")
+//            })
             
-            let request = FBSDKLoginKit.GraphRequest(graphPath: "me",
-                                                     parameters: ["fields":"email,name"],
-                                                     tokenString: token,
-                                                     version: nil,
-                                                     httpMethod: .get)
-            request.start(completionHandler: {connection,result, error in
-                print("\(result)")
-            })
-            
-            self.performSegue(withIdentifier: "fbLogin", sender: self)
-            }
+//            self.performSegue(withIdentifier: "fbLogin", sender: self)
+//            }
         
-        else
-            {
-        let loginButton = FBLoginButton()
-        
-        
-                
-      
-        loginButton.center = view.center
-                loginButton.layer.cornerRadius = 10
-        loginButton.delegate = self
-        loginButton.permissions = ["public_profile", "email"]
-        view.addSubview(loginButton)
+//        else
+//            {
+//        let loginButton = FBLoginButton()
+//
+//
+//
+//
+//        loginButton.center = view.center
+//                loginButton.layer.cornerRadius = 10
+//        loginButton.delegate = self
+//        loginButton.permissions = ["public_profile", "email"]
+//        view.addSubview(loginButton)
         }
         
-        logIn.layer.cornerRadius = 17
-        register.layer.cornerRadius = 17
-      
-        
-        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationController?.navigationBar.shadowImage = UIImage()
-        
-        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+//        logIn.layer.cornerRadius = 17
+//        register.layer.cornerRadius = 17
+//      
+//        
+//        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+//        navigationController?.navigationBar.shadowImage = UIImage()
+//        
+//        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
     }
     
     
@@ -84,7 +84,7 @@ class WelcomeView: UIViewController,LoginButtonDelegate  {
   
     
     
-}
+
 
 
 
